e6b1855443957248fff20896d2beaae69df13707
Terence Parr <parrt@antlr.org>
2022-07-05 16:25:11 -0700

    send back errors now

--- src/org/antlr/v4/server/ANTLRHttpServer.java
+++ src/org/antlr/v4/server/ANTLRHttpServer.java
@@ -1,39 +1,60 @@
 package org.antlr.v4.server;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.antlr.runtime.RecognitionException;
-import org.antlr.v4.Tool;
-import org.antlr.v4.gui.Interpreter;
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.atn.DecisionInfo;
 import org.antlr.v4.runtime.atn.ParseInfo;
+import org.antlr.v4.runtime.misc.Utils;
 import org.antlr.v4.runtime.tree.JsonSerializer;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.tool.*;
-import org.eclipse.jetty.http.HttpParser;
 import org.eclipse.jetty.server.*;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
-import org.eclipse.jetty.webapp.WebAppContext;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ANTLRHttpServer {
+	static class GrammarErrorListener implements ANTLRToolListener {
+		List<ANTLRMessage> msgs = new ArrayList<>();
+		@Override
+		public void info(String msg) { }
+		@Override
+		public void error(ANTLRMessage msg) {
+			msgs.add(msg);
+		}
+		@Override
+		public void warning(ANTLRMessage msg) {
+			msgs.add(msg);
+		}
+	}
+
+	static class ParseErrorListener extends BaseErrorListener {
+		List<String> msgs = new ArrayList<>();
+		@Override
+		public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
+								int line, int charPositionInLine,
+								String msg,
+								org.antlr.v4.runtime.RecognitionException e)
+		{
+			msg = Utils.escapeJSONString(msg);
+			String err;
+			if ( offendingSymbol != null) {
+				err = String.format("{\"token\":%d,\"line\":%d,\"pos\":%d,\"msg\":\"%s\"}",
+						((Token) (offendingSymbol)).getTokenIndex(), line, charPositionInLine, msg);
+			}
+			else {
+				err = String.format("{\"line\":%d,\"pos\":%d,\"msg\":\"%s\"}",
+						line, charPositionInLine, msg);
+			}
+			msgs.add(err);
+		}
+	}
+
 	protected static class IgnoreTokenVocabGrammar extends Grammar {
 		public IgnoreTokenVocabGrammar(String fileName,
 									   String grammarText,
@@ -63,9 +84,9 @@ public class ANTLRHttpServer {
 			String grammar = request.getParameter("grammar");
 			String input = request.getParameter("input");
 			String startRule = request.getParameter("start");
-			System.out.println(grammar);
-			System.out.println(input);
-			System.out.println(startRule);
+//			System.out.println(grammar);
+//			System.out.println(input);
+//			System.out.println(startRule);
 //			JsonReader jsonReader = Json.createReader(new StringReader(data));
 //			JsonObject jobj = jsonReader.readObject();
 //			System.out.println(jobj);
@@ -85,9 +106,11 @@ public class ANTLRHttpServer {
 		startRule = startRule.strip();
 		Grammar g = null;
 		LexerGrammar lg = null;
-		DefaultToolListener listener = new DefaultToolListener(new Tool());
+//		DefaultToolListener listener = new DefaultToolListener(new Tool());
+		GrammarErrorListener listener = new GrammarErrorListener();
 		try {
 			g = new IgnoreTokenVocabGrammar(null, grammar, null, listener);
+			System.out.println("grammar errors" + listener.msgs);
 		}
 		catch (RecognitionException re) {
 			System.err.println("Can't parse grammar");
@@ -99,11 +122,19 @@ public class ANTLRHttpServer {
 				lg.createLexerInterpreter(charStream) :
 				g.createLexerInterpreter(charStream);
 
+		ParseErrorListener lexListener = new ParseErrorListener();
+		lexEngine.removeErrorListeners();
+		lexEngine.addErrorListener(lexListener);
+
 		CommonTokenStream tokens = new CommonTokenStream(lexEngine);
 
 		tokens.fill();
 
 		GrammarParserInterpreter parser = g.createGrammarParserInterpreter(tokens);
+
+		ParseErrorListener parseListener = new ParseErrorListener();
+		parser.removeErrorListeners();
+		parser.addErrorListener(parseListener);
 
 		Rule r = g.rules.get(startRule);
 		if (r == null) {
@@ -113,10 +144,18 @@ public class ANTLRHttpServer {
 		ParseTree t = parser.parse(r.index);
 		ParseInfo parseInfo = parser.getParseInfo();
 
+		System.out.println("lex msgs" + lexListener.msgs);
+		System.out.println("parse msgs" + parseListener.msgs);
+
+		System.out.println(t.toStringTree(parser));
+
 		String json = JsonSerializer.toJSON(t, parser);
 //		System.out.println(json);
 
-		return json;
+		return String.format("{\"lerrors\":[%s],\"perrors\":[%s],\"result\":%s}",
+				String.join(",", lexListener.msgs),
+				String.join(",", parseListener.msgs),
+				json);
 	}
 
 	public static void main(String[] args) throws Exception {
